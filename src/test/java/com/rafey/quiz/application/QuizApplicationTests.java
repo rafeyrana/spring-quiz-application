aa69c7590bb4cf9788760222663042b3c04e33b3
Rafey Rana <24100103@lums.edu.pk>
2023-08-10 10:12:35 +0500

    App

---
+++ src/test/java/com/rafey/quiz/application/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafey.quiz.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
