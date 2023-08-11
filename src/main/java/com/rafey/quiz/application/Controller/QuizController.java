1f1602e23ae8a3946d01ffa9536981e8882f72c8
Rafey Rana <24100103@lums.edu.pk>
2023-08-11 16:04:43 +0500

    GET QUIZ

--- src/main/java/com/rafey/quiz/application/Controller/QuizController.java
+++ src/main/java/com/rafey/quiz/application/Controller/QuizController.java
@@ -1,10 +1,14 @@
 package com.rafey.quiz.application.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.rafey.quiz.application.Service.QuizService;
+import com.rafey.quiz.application.Model.*;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,6 +23,12 @@ public class QuizController {
             @RequestParam String title) {
         ResponseEntity<String> response = quizService.createQuiz(category, number_of_questions, title);
         return new ResponseEntity<>(response.getBody(), response.getHeaders(), response.getStatusCode());
+    }
+
+    @GetMapping("/get/{id}") 
+    public ResponseEntity<List<Question>> getQuiz(@PathVariable Integer id) {
+        List<Question> questions = quizService.getQuiz(id);
+        return new ResponseEntity<>(questions, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/rafey/quiz/application/Service/QuizService.java
+++ src/main/java/com/rafey/quiz/application/Service/QuizService.java
@@ -1,6 +1,8 @@
 package com.rafey.quiz.application.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.HttpStatus;
@@ -19,14 +21,22 @@ public class QuizService {
     @Autowired
     QuestionDao questionDao;
 
-    public ResponseEntity <String> createQuiz(String category, Integer number_of_questions, String title) {
+    public ResponseEntity<String> createQuiz(String category, Integer number_of_questions, String title) {
         Quiz quiz = new Quiz();
         quiz.setTitle(title);
-        List<Question> questions = questionDao.getRandomQuestionsByCategory(category,number_of_questions);
+        List<Question> questions = questionDao.getRandomQuestionsByCategory(category, number_of_questions);
         quiz.setQuestions(questions);
         System.out.println(quiz);
         quizdao.save(quiz);
-        return new ResponseEntity<String>("success",HttpStatus.OK);
+        return new ResponseEntity<String>("success", HttpStatus.OK);
+    }
+
+    public List<Question> getQuiz(Integer id) {
+        Optional<Quiz> quiz = quizdao.findById(id);
+        if (quiz.isPresent()) {
+            return quiz.get().getQuestions();
+        }
+        return new ArrayList<>(); // Return an empty list if quiz is not found
     }
 
 }
